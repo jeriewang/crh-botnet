18d0d8bda41cadcddaab666fa64c0cec451826e6
Ruocheng Wang <yrrejkk@gmail.com>
2019-04-24 00:09:54 -0400

    Network Implementation Completed

--- crh_botnet/network.py
+++ crh_botnet/network.py
@@ -1,78 +1,8 @@
-import asyncio, aiohttp, sys
+import asyncio, aiohttp, sys,requests
 from .message import Message
 from typing import Union, List
 from types import FunctionType
 
-
-class AsyncMethodsWrapper:
-    def __init__(self, network):
-        """
-        :param RobotNetwork robot: The network
-        """
-        self._network = network
-
-    async def connect(self):
-        """
-        This function is a coroutine. Connects to the network.
-        :return: None
-        """
-
-    async def poll(self) -> List[int]:
-        """
-        This function is a a coroutine.
-
-        :return: A list of message IDs.
-        :rtype: list[int]
-        """
-        raise NotImplementedError
-
-    async def send(self, msg: Message, recipient: int):
-        """
-        This function is a a coroutine.
-
-        :param Message msg: The message for sending
-        :param int recipient: The ID of the recipient.
-        :return: The message sent (for callback purpose)
-        :rtype: Message
-        """
-        raise NotImplementedError
-    
-    async def broadcast(self, msg: Message):
-        """
-        This function is a a coroutine. Broadcast a message to the entire robot network
-        :param Message msg: The message for broadcasting
-        :return: The message object broadcast (for callback purpose)
-        :rtype: Message
-        """
-
-    async def disconnect(self):
-        """
-        This function is a a coroutine. Disconnect from the network.
-
-        :return: None
-        """
-        raise NotImplementedError
-
-    async def retrieve(self, msg_id: int):
-        """
-        This function is a coroutine.
-
-        :param int msg_id: The message id
-        :return: The retrieved message
-        :rtype: Message
-        """
-        raise NotImplementedError
-
-
-    async def get_connected_robots(self):
-        """
-        This function is a coroutine. Get all robots that are currently connected
-        to the network.
-        :return: A list of robot IDs.
-        :rtype: list[int]
-        """
-    
-    
 class RobotNetwork:
     """
     The networking interface that enable robots to communicate with each other.
@@ -80,7 +10,7 @@ class RobotNetwork:
     or to broadcast a message to every single robot in the network.
     """
     
-    SERVER_ADDR='localhost:5001'
+    SERVER_ADDR='http://localhost:5003'
     
     def __init__(self, robot):
         """
@@ -90,6 +20,7 @@ class RobotNetwork:
         self._is_connected = asyncio.Event()
         self.coro=AsyncMethodsWrapper(self)
         self.connected_robots=[]
+        self.token=None
     
     @property
     def is_connected(self):
@@ -101,6 +32,15 @@ class RobotNetwork:
         
         :return: None
         """
+        
+        # This function cannot be async because it is called before the event
+        # loop is running.
+        r=requests.post(f'{self.SERVER_ADDR}/api/connect',json={'id':self._robot.id})
+        r.raise_for_status()
+        self.token=r.json()['token']
+        #print(self.token)
+        self._is_connected.set()
+        
     
     
     def send(self, msg: Message, recipient: int, callback=None):
@@ -146,7 +86,7 @@ class RobotNetwork:
         :param msg: A :class:`Message` object for broadcasting
         :return: None
         """
-        asyncio.ensure_future(self.broadcast(msg))
+        asyncio.ensure_future(self.coro.broadcast(msg))
     
     
     def disconnect(self):
@@ -154,7 +94,8 @@ class RobotNetwork:
         Disconnect from the network
         :return:
         """
-
+        r = requests.post(f'{self.SERVER_ADDR}/api/disconnect', headers={'Authorization':'Token '+self.token})
+        r.raise_for_status()
 
     @classmethod
     def set_server_address(cls, addr):
@@ -165,3 +106,100 @@ class RobotNetwork:
     
         cls.SERVER_ADDR = addr
 
+
+class AsyncMethodsWrapper:
+    def __init__(self, network):
+        """
+        :param RobotNetwork network: The network
+        """
+        self._network = network
+        self._session = None
+    
+    async def poll(self) -> List[Message]:
+        """
+        This function is a a coroutine.
+
+        :return: A list of messages.
+        :rtype: list[Message]
+        """
+        if not self._session:
+            await self._create_session()
+        
+        res=await self._session.get(self._network.SERVER_ADDR+'/api/poll')
+        obj=await res.json()
+        self._network.connected_robots=obj['robots']
+        ret=[]
+        for m in obj['messages']:
+            ret.append(Message.from_dict(m))
+        return ret
+    
+    async def send(self, msg: Message, recipient: int):
+        """
+        This function is a a coroutine.
+
+        :param msg: The message for sending
+        :type msg: Message or str
+        :param int recipient: The ID of the recipient.
+        :return: The message sent (for callback purpose)
+        :rtype: Message
+        """
+        if not self._session:
+            await self._create_session()
+        
+        if isinstance(msg,str):
+            msg=Message(msg)
+        assert isinstance(msg,Message)
+        msg.set_recipient(recipient)
+        msg.set_sender(self._network._robot.id)
+        await self._session.put(self._network.SERVER_ADDR+'/api/send',json=msg.to_dict())
+        return msg
+    
+    async def broadcast(self, msg):
+        """
+        This function is a a coroutine. Broadcast a message to the entire robot network
+        :param msg: The message for broadcasting
+        :type msg: Message or str
+        :return: The message object broadcast (for callback purpose)
+        :rtype: Message
+        """
+        if not self._session:
+            await self._create_session()
+            
+        if isinstance(msg,str):
+            msg=Message(msg)
+        assert isinstance(msg,Message)
+        msg.set_recipient(-1)
+        msg.set_sender(self._network._robot.id)
+        await self._session.put(self._network.SERVER_ADDR+'/api/send',json=msg.to_dict())
+        return msg
+    
+    async def disconnect(self):
+        """
+        This function is a a coroutine. Disconnect from the network.
+
+        :return: None
+        """
+        if not self._session:
+            await self._create_session()
+        await self._session.post(self._network.SERVER_ADDR+'/api/disconnect')
+    
+    async def retrieve(self, msg_id: int):
+        """
+        This function is a coroutine.
+
+        :param int msg_id: The message id
+        :return: The retrieved message
+        :rtype: Message
+        """
+        if not self._session:
+            await self._create_session()
+        
+        raise NotImplementedError
+    
+    async def _create_session(self):
+        if not self._network.is_connected:
+            raise ConnectionError('The RobotNetwork is not connected')
+        self._session = aiohttp.ClientSession(
+                raise_for_status=True,
+                headers={'Authorization': 'Token ' + self._network.token}
+        )
