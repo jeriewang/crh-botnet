eae27329b7e64aa6119c07581b1ee9cdaed53f99
Ruocheng Wang <yrrejkk@gmail.com>
2019-04-24 00:10:36 -0400

    Message Implementation Completed

--- crh_botnet/message.py
+++ crh_botnet/message.py
@@ -1,4 +1,4 @@
-import time, json
+import time, json,datetime
 
 
 class Message:
@@ -18,8 +18,8 @@ class Message:
         self.sender = None
         "ID of the message sender, :class:`int`"
         
-        self.time_created = time.time()
-        "Creation time of the message. UNIX timestamp. :class:`float`. "
+        self.time_created = datetime.datetime.now()
+        "Creation time of the message. :class:`datetime.datetime`. "
         
         self.recipient = None
         "ID of the recipient of the message, :class:`int`"
@@ -51,8 +51,39 @@ class Message:
         msg = Message(str(obj['content']))
         msg.set_recipient(int(obj['recipient']))
         msg.set_sender(int(obj['sender']))
-        msg.time_created = float(obj['time_created'])
+        msg.time_created = datetime.datetime.utcfromtimestamp(float(obj['time_created']))
         return msg
+    
+    @classmethod
+    def from_dict(cls, d: dict):
+        """
+        :param dict d: The message
+        :return: The message.
+        :rtype: Message
+        """
+    
+        msg = Message(str(d['content']))
+        msg.set_recipient(int(d['recipient']))
+        msg.set_sender(int(d['sender']))
+        msg.time_created = datetime.datetime.utcfromtimestamp(float(d['time_created']))
+        return msg
+
+    @classmethod
+    def from_db_record(cls, record):
+        """
+        :param record: A message retrieved from db.
+        :type record: sqlite3.Row
+        :return: The message.
+        :rtype: Message
+        """
+        
+        msg=cls(record['content'])
+        msg.set_sender(record['sender'])
+        msg.set_recipient(record['recipient'])
+        msg.time_created=record['time_created']
+        
+        return msg
+        
     
     def to_json(self):
         """
@@ -68,8 +99,33 @@ class Message:
             'content'     : self.content,
             'sender'      : self.sender,
             'recipient'   : self.recipient,
-            'time_created': self.time_created
+            'time_created': self.time_created.timestamp()
         })
+
+    def to_dict(self):
+        """
+        :return: The message as a JSON serializable dict.
+        :rtype: dict
+        """
+        if self.recipient is None:
+            raise ValueError('Message recipient cannot be None')
+        if self.sender is None:
+            raise ValueError('Message sender cannot be None')
+    
+        return {
+            'content'     : self.content,
+            'sender'      : self.sender,
+            'recipient'   : self.recipient,
+            'time_created': self.time_created.timestamp()
+        }
+    
+    
+    def validate(self):
+        """
+        Check if the message is valid
+        :rtype: bool
+        """
+        return self.recipient is not None and self.sender is not None
     
     def __eq__(self, other):
         if not isinstance(other, Message):
