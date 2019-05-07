57bd194e9f93c2f3a4d10f5d086887755fb1b59d
Ruocheng Wang <yrrejkk@gmail.com>
2019-05-06 20:19:37 -0400

    Updated Documentations

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,6 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../'))
 from crh_botnet import __version__
 
@@ -20,58 +9,43 @@ from crh_botnet import __version__
 project = 'CRH BotNet'
 copyright = '2019, Jerry Wang'
 author = 'Jerry Wang'
-
-# The full version, including alpha/beta/rc tags
 release = __version__
-
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-	'sphinx.ext.autodoc',
-	'sphinx.ext.intersphinx',
-	'sphinx_sitemap'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx_sitemap'
 ]
 
-autodoc_member_order='bysource'
-#autoclass_content='both'
+autodoc_member_order = 'bysource'
+# autoclass_content='both'
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'alabaster'
 html_theme_options = {
-	'show_powered_by':False,
-	'canonical_url':'docs.jerrywang.website/crh-botnet/',
-	'analytics_id':'UA-115911413-2',
-	'github_user':'pkqxdd',
-	'github_repo': 'crh-botnet',
-	'github_button':'true',
-	'github_type':'star',
-	'github_count':'false'
+    'show_powered_by': False,
+    'canonical_url'  : 'docs.jerrywang.website/crh-botnet/',
+    'analytics_id'   : 'UA-115911413-2',
+    'github_user'    : 'pkqxdd',
+    'github_repo'    : 'crh-botnet',
+    'github_button'  : 'true',
+    'github_type'    : 'star',
+    'github_count'   : 'false'
 }
-html_baseurl='https://docs.jerrywang.website/crh-botnet/'
+html_baseurl = 'https://docs.jerrywang.website/crh-botnet/'
+html_static_path = ['_static', '_static/custom.css']
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static','_static/custom.css']
+intersphinx_mapping = {
+    'python'  : ('https://docs.python.org/3', None),
+    'gpiozero': ('https://gpiozero.readthedocs.io/en/stable/', None)
+}
 
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 def setup(app):
-	app.add_stylesheet('custom.css')
+    app.add_stylesheet('custom.css')
---
+++ docs/examples/button_control_controller.py
@@ -0,0 +1,16 @@
+from crh_botnet import *
+from gpiozero import Button
+from functools import partial
+
+robot = Robot()
+robot.network.set_server_address("choate-robotics-rpi-01.local")
+
+
+def setup():
+    print("Connected to the robot network")
+    button = Button(15)
+    button.when_pressed = partial(robot.network.send, 'on', 0)
+    button.when_released = partial(robot.network.send, 'off', 0)
+
+
+robot.run(globals())
